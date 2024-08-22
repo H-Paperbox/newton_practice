de617770b20479a935ed8becd4873e482b479233
Helen-LZH <zihe_liu@berkeley.edu>
2024-08-22 13:35:43 -0700

    documentation added newton module

--- newton2.py
+++ newton2.py
@@ -1,16 +1,23 @@
-import numpy as np
-# def f(x):
-#     return np.cos(x)
-def f1(x,f):
-    return f(x+1e-4)-f(x)/1e-4
-def f2(x,f):
-    return f1(x+1e-4,f)-f1(x,f)/1e-4
+## First order derivative
+def f1(x, f):
+    return f(x + 1e-4) - f(x) / 1e-4
 
-min = 1e-4
+
+## Second order derivative
+def f2(x, f):
+    return f1(x + 1e-4, f) - f1(x, f) / 1e-4
+
+
+## The newton's law of finding optimal x
 def find(x, f):
-    x_pre = x - 2 ##
-    while (x-x_pre > min):
+    min = 1e-4
+    x_pre = x - 2  ##
+    while x - x_pre > min:
         x_pre = x
-        x = x_pre - f1(x_pre,f)/f2(x_pre,f) 
-    # print(x)
+        x = x_pre - f1(x_pre, f) / f2(x_pre, f)
     return x
+
+
+## Example
+import numpy as np
+print(find(2.5, np.cos))
