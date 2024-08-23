918a2b50616a6cbdc0677e3d00592a2d44e0e3ec
Helen-LZH <zihe_liu@berkeley.edu>
2024-08-23 09:38:15 -0700

    multi-newton

---
+++ multi_newton.py
@@ -0,0 +1,13 @@
+import autograd.numpy as np
+from autograd import hessian
+
+def find_multi(x,f):
+    min = 1e-4
+    delta = 2
+    while delta > min:
+        first_derivative = np.gradient(x)
+        hess = hessian(x)
+        del_x = hess.T * first_derivative
+        delta = sum( del_x**2 )
+        x = x - del_x
+    return x
